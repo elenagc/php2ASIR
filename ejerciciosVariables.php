d28d54a9f954af3ede5a69277b2dadb692bfcea9
elenagc <elenagc84@gmail.com>
2024-11-12 11:53:06 +0100

    Ejercicios corregidos el jueves 7/11/2024

--- ejerciciosVariables.php
+++ ejerciciosVariables.php
@@ -11,7 +11,6 @@ $edad = 25;
 $ciudad = "Madrid";
 
 print "<h2> Hola, mi nombre es $nombre, tengo $edad años y vivo en $ciudad.</h2>";
-
 /**
  *  2. Cambiar Valores de Variables
  *  
@@ -25,6 +24,7 @@ $edad = 20;
 
 print "<h2> Nombre: $nombre, Color favorito: $colorFavorito, Edad: $edad </h2>";
 
+	
 // Cambiando valores
 $nombre = "Carlos";
 $colorFavorito = "verde";
@@ -33,29 +33,38 @@ $edad = 25;
 print "<h2> Nombre: $nombre, Color favorito: $colorFavorito, Edad: $edad </h2>";
 
 print "<h1>----EJERCICIO 3----</h1>";
-
+/**
+ *  3. Calcula y muestra cómo sería la edad en 5, 10 y 15 años usando variables
+ * 
+ *  */	
 $edadActual = 30;
 
 print "<h2> Edad actual: $edadActual años </h2>";
+$edadActual= $edadActual +5 ;
 print "<h2> Edad en 5 años: " . ($edadActual + 5) . "</h2>";
+$edadActual= $edadActual + 10;
 print "<h2> Edad en 10 años: " . ($edadActual + 10) . "</h2>";
 print "<h2> Edad en 15 años: " . ($edadActual + 15) . "</h2>";
 
 print "<h1>----EJERCICIO 4----</h1>";
+/**
+ *  4. Calcula el salario tras un incremento del 5% anual durante tres años
+ * 
+ *  */	
 
 $salario = 1000;
+//  5% 
 $incremento = 0.05;
 
 print "<h2> Salario inicial: $salario </h2>";
-
-// Año 1
-$salario = $salario * (1 + $incremento);
+$incremento = $salario * 0.05; 
+$salario = $salario + $incremento;
 print "<h2> Salario después de 1 año: $salario </h2>";
+$incremento = $salario * 0.05; 
+$salario = $salario + $incremento;
+print "<h2> Salario después de 2 año: $salario </h2>";
+$incremento = $salario * 0.05; 
+$salario = $salario + $incremento;
+print "<h2> Salario después de 3 año: $salario </h2>";
 
-// Año 2
-$salario = $salario * (1 + $incremento);
-print "<h2> Salario después de 2 años: $salario </h2>";
-
-// Año 3
-$salario = $salario * (1 + $incremento);
-print "<h2> Salario después de 3 años: $salario </h2>";
+?>
