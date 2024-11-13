a2efca9becf228f3b34edea83df9ea845c7eb378
elenagc <elenagc84@gmail.com>
2024-11-13 18:51:04 +0100

    Practica 5.2. ejercicio 10 corregido el miercoles 13/11/2024

---
+++ practica5.2.Ejercicio10-php y html.php
@@ -0,0 +1,14 @@
+<?php
+print '<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Generada con PHP</title>
+</head>
+<body>
+    <h1>¡Esta es una página generada con PHP!</h1>
+    <p>Este contenido está escrito dentro de una estructura HTML generada completamente con PHP.</p>
+</body>
+</html>';
+?>
