d7b6e85adf0aa2b85eb136b1c6d9ed636f02ab56
elenagc <elenagc84@gmail.com>
2024-11-13 18:50:44 +0100

    Practica 5.2. ejercicio 9 corregido el miercoles 13/11/2024

---
+++ practica5.2.Ejercicio9-php y html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo de PHP en HTML</title>
+</head>
+<body>
+    <h1>
+        <?php
+        print "¡Bienvenidos a nuestra página con PHP!";
+        ?>
+    </h1>
+
+    <h2>
+    <?php
+        print "¡Bienvenidos a nuestra página con PHP!";
+        ?>
+
+    </h2>
+</body>
+</html>
